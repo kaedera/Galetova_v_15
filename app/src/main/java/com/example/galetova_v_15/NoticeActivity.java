85609342878c3656f5f14cc6ff148e6a0d5a001d
kaede <angela.galetova@gmail.com>
2023-12-26 13:49:02 +0700

    Galetova_v_15_2

--- app/src/main/java/com/example/galetova_v_15/NoticeActivity.java
+++ app/src/main/java/com/example/galetova_v_15/NoticeActivity.java
@@ -2,7 +2,9 @@ package com.example.galetova_v_15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NoticeActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class NoticeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notice);
+    }
+    public void ArrowBtn(View v){
+        Intent intent = new Intent(this, PersonalAreaActivity.class);
+        startActivity(intent);
     }
 }
